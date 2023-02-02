5ab972ecb0177aec301526555792749398a77306
isyekong <yekongtat@gmail.com>
2023-02-03 04:38:56 +0800

    完善脚本判断：如果更新失败则不写入文件，防止 bird 重载错误

--- node/peers/down_prefix/update.sh
+++ node/peers/down_prefix/update.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 cd /etc/bird/peers/down_prefix
-update_prefix () { 
-  if bgpq4 -6 -b -R 48 -l prefix_$1 $2 > $1.conf.tmp; then
-    echo -n "define " > $1.conf
-    cat $1.conf.tmp >> $1.conf
-    rm $1.conf.tmp
-  else
-    echo "$1 update failed"
+update_prefix () {
+  bgpq4 -S RPKI,RIPE,APNIC,ARIN,AFRINIC,LACNIC -6 -b -R 48 -l prefix_$1 $2 > $1.conf.prefix
+  if [ ! -s $1.conf.prefix ]; then
+    echo "$1 $2 update failed"
+    return
   fi
-  if bgpq4 -tb -l asn_$1 $2 > $1.conf.tmp; then
-    echo "" >> $1.conf
-    echo -n "define " >> $1.conf
-    cat $1.conf.tmp >> $1.conf
-    rm $1.conf.tmp
-  else
-    echo "$1 update failed"
+  bgpq4  -S RPKI,RIPE,APNIC,ARIN,AFRINIC,LACNIC -tb -l asn_$1 $2 > $1.conf.asns
+  if [ ! -s $1.conf.asns ]; then
+    echo "$1 $2 update failed"
+    return
   fi
-}
 
+  echo -n "define " > $1.conf
+  cat $1.conf.prefix >> $1.conf
+  echo "" >> $1.conf
+  echo -n "define " >> $1.conf
+  cat $1.conf.asns >> $1.conf
+
+  rm $1.conf.prefix
+  rm $1.conf.asns
+}
 update_prefix 138211 as-moeqing
 
